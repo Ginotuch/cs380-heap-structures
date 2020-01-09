dee81d5b933f91eea40c06f1dc2f10323cd27af8
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2020-01-09 13:50:36 +1300

    moved some test utility functions to separate file

--- tests/test_heapsorting.py
+++ tests/test_heapsorting.py
@@ -1,7 +1,8 @@
-import unittest
-from typing import Tuple, List, Any
-from heap import Heap
 import random
+import unittest
+
+import tests.util as util
+from heap import Heap
 
 
 class TestSorting(unittest.TestCase):
@@ -10,41 +11,41 @@ class TestSorting(unittest.TestCase):
     def test_simple_list(self):
         heap_instance = Heap()
         list_to_test = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
-        retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+        retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
         self.assertEqual(list_to_test, retrieved_list)
 
     def test_simple_reversed_list(self):
         heap_instance = Heap()
         list_to_test = [(5, 5), (4, 4), (3, 3), (2, 2), (1, 1)]
-        retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+        retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
         self.assertEqual(list_to_test[::-1], retrieved_list)
 
     def test_simple_initialised_list(self):
         list_to_test = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
         heap_instance = Heap(list_to_test[:])
-        retrieved_list = TestSorting.retrieve(heap_instance)
+        retrieved_list = util.retrieve(heap_instance)
         self.assertEqual(list_to_test, retrieved_list)
 
     def test_random_list(self):
         random.seed(self._seed)
         for x in range(20):
             heap_instance = Heap()
-            list_to_test = TestSorting.get_random_list()
-            retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+            list_to_test = util.get_random_list()
+            retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
             self.assertEqual(sorted(list_to_test), retrieved_list, "Random seed: ({})".format(str(self._seed)))
 
     def test_random_list_same_instance(self):
         random.seed(self._seed)
         heap_instance = Heap()
         for x in range(20):
-            list_to_test = TestSorting.get_random_list()
-            retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+            list_to_test = util.get_random_list()
+            retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
             self.assertEqual(sorted(list_to_test), retrieved_list, "Random seed: ({})".format(str(self._seed)))
 
     def test_random_deletion(self):
         random.seed(self._seed)
         for x in range(20):
-            list_to_test = TestSorting.get_random_list()
+            list_to_test = util.get_random_list()
             heap_instance = Heap(list_to_test[:])
             heap_instance._check_heap()
             index_to_delete = random.randrange(0, len(list_to_test))
@@ -52,7 +53,7 @@ class TestSorting(unittest.TestCase):
             heap_instance.remove(deleted_element[1])
             heap_instance._check_heap()
             self.assertEqual(heap_instance.peek(), min(list_to_test))
-            retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+            retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
             self.assertEqual(sorted(list_to_test), retrieved_list, "Random seed: ({})".format(str(self._seed)))
 
     def test_random_deletion_same_insatnce(self):
@@ -63,7 +64,7 @@ class TestSorting(unittest.TestCase):
         """
         random.seed(self._seed)
         for run in range(20):
-            list_to_test = TestSorting.get_random_list()
+            list_to_test = util.get_random_list()
             heap_instance = Heap(list_to_test[:])
             heap_instance._check_heap()
             for deletion in range(random.randrange(10, 90)):
@@ -72,26 +73,8 @@ class TestSorting(unittest.TestCase):
                 heap_instance.remove(deleted_element[1])
                 heap_instance._check_heap()
             self.assertEqual(heap_instance.peek(), min(list_to_test))
-            retrieved_list = TestSorting.insert_and_extract(heap_instance, list_to_test)
+            retrieved_list = util.insert_and_extract(heap_instance, list_to_test)
             self.assertEqual(sorted(list_to_test), retrieved_list, "Random seed: ({})".format(str(self._seed)))
-
-    @staticmethod
-    def get_random_list() -> List[Tuple[Any, Any]]:
-        return [(x, x) for x in random.sample(range(1, 10 ** 9), random.randrange(10**2, 10 ** 3))]
-
-    @staticmethod
-    def insert_and_extract(heap_instance: Heap, list_to_insert: List[Tuple[Any, Any]]) -> List[Tuple[Any, Any]]:
-        for number_tuple in list_to_insert:
-            heap_instance.push(number_tuple[0], number_tuple[1])
-        heap_instance._check_heap()
-        return TestSorting.retrieve(heap_instance)
-
-    @staticmethod
-    def retrieve(heap_instance: Heap) -> List[Tuple[Any, Any]]:
-        retrieved_list: List[Tuple[Any, Any]] = []
-        while heap_instance.size > 0:
-            retrieved_list.append(heap_instance.pop())
-        return retrieved_list
 
 
 if __name__ == '__main__':
---
+++ tests/util.py
@@ -0,0 +1,22 @@
+import random
+from typing import Tuple, List, Any
+
+from heap import Heap
+
+
+def get_random_list() -> List[Tuple[Any, Any]]:
+    return [(x, x) for x in random.sample(range(1, 10 ** 9), random.randrange(10 ** 2, 10 ** 3))]
+
+
+def insert_and_extract(heap_instance: Heap, list_to_insert: List[Tuple[Any, Any]]) -> List[Tuple[Any, Any]]:
+    for number_tuple in list_to_insert:
+        heap_instance.push(number_tuple[0], number_tuple[1])
+    heap_instance._check_heap()
+    return retrieve(heap_instance)
+
+
+def retrieve(heap_instance: Heap) -> List[Tuple[Any, Any]]:
+    retrieved_list: List[Tuple[Any, Any]] = []
+    while heap_instance.size > 0:
+        retrieved_list.append(heap_instance.pop())
+    return retrieved_list
